8743f56e8c3d724fcf648ee5916678597fced9b0
devansh <devanshbaghel059@gmail.com>
2026-09-15 15:34:15 +0530

    fortmatting mac address

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ import textwrap
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
-#6s for six bytes or characters , H for unisgned integer
+#6s for six bytes or characters , H for unsigned integer 
+
+#properly formatting the mac address
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    mac_addr = ':'.join(bytes_str).upper()
+    return mac_addr
