02a33fc280e8496d7d7f10863291a9d3d2e8a1c5
devansh <devanshbaghel059@gmail.com>
2026-09-16 19:45:36 +0530

    unpacking icmp packet and tcp segment

--- main.py
+++ main.py
@@ -30,9 +30,29 @@ def ipv4_packet(data):
     ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data [:20])
     return version, header_length, ttl, proto, ipv4(src), ipv4(target),
 
+
+#formatting the ipv4 address
 def ipv4(addr):
     return '.'.join(map(str, addr))
 
+
+#unpacking icmp packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+
+#unpacking tcp segment
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgment, offset_reserved_flags) =struct.unpack(' ! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:] 
 
 
 
