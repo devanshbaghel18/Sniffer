b15b739daca565514863fa7f613e1c07489e5af6
devansh <devanshbaghel059@gmail.com>
2026-09-16 18:36:52 +0530

    unpacking a ipv4 packet header and properly formatting it

--- main.py
+++ main.py
@@ -23,4 +23,19 @@ def get_mac_addr(bytes_addr):
     mac_addr = ':'.join(bytes_str).upper()
     return mac_addr
 
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data [:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target),
+
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+
+
+
+
+
 main()
