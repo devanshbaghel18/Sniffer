eb704be2ddbadcc09cd2f1e948d10f8f779d8dae
devansh <devanshbaghel059@gmail.com>
2026-09-16 17:54:52 +0530

    capturing traffic works

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import socket
 import struct
 import textwrap
 
+def main():
+    conn =socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    while True:
+        raw_data,addr=conn.recvfrom(65535)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame:')
+        print(f'Destination: {dest_mac}, Source: {src_mac}, Protocol: {eth_proto}')
+
+
 #unpacking ethernet frame
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
@@ -13,3 +22,5 @@ def get_mac_addr(bytes_addr):
     bytes_str = map('{:02x}'.format, bytes_addr)
     mac_addr = ':'.join(bytes_str).upper()
     return mac_addr
+
+main()
