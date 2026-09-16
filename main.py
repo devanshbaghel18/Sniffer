df981189d21e4801d86bb6dbedebade2d96c165c
devansh <devanshbaghel059@gmail.com>
2026-09-16 20:01:21 +0530

    added udp and formatted the line so it is easy to read and explains what is getting print easily

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import socket
 import struct
 import textwrap
 
+TAB_1 = '\t - '
+TAB_2 = '\t\t - '
+TAB_3 = '\t\t\t - '
+TAB_4 = '\t\t\t\t - '
+
+DATA_TAB_1 = '\t - Data: '
+DATA_TAB_2 = '\t\t - Data: '
+DATA_TAB_3 = '\t\t\t - Data: '
+DATA_TAB_4 = '\t\t\t\t - Data: '
+
+
+
 def main():
     conn =socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     while True:
@@ -9,6 +21,34 @@ def main():
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print('\nEthernet Frame:')
         print(f'Destination: {dest_mac}, Source: {src_mac}, Protocol: {eth_proto}')
+
+        if eth_proto == 8:
+            (version, header_length, ttl, proto, src, target,data) = ipv4_packet(data)
+            print(TAB_1 + 'IPv4 Packet:')
+            print(TAB_2 + f'Version: {version}, Header Length: {header_length}, TTL: {ttl}')
+            print(TAB_2 + f'Protocol: {proto}, Source: {src}, Target: {target}')                
+
+            if proto == 1:
+                icmp_type, code, checksum, data = icmp_packet(data)
+                print(TAB_1 + 'ICMP Packet:')
+                print(TAB_2 + f'Type: {icmp_type}, Code: {code}, Checksum: {checksum}')
+                print(format_multi_line(DATA_TAB_2, data))
+
+            elif proto == 6:
+                src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data = tcp_segment(data)
+                print(TAB_1 + 'TCP Segment:')
+                print(TAB_2 + f'Source Port: {src_port}, Destination Port: {dest_port}')
+                print(TAB_2 + f'Sequence: {sequence}, Acknowledgment: {acknowledgment}')
+                print(TAB_2 + f'Flags:')
+                print(TAB_3 + f'URG: {flag_urg}, ACK: {flag_ack}, PSH: {flag_psh}')
+                print(TAB_3 + f'RST: {flag_rst}, SYN: {flag_syn}, FIN: {flag_fin}')
+                print(format_multi_line(DATA_TAB_2, data))
+
+            elif proto == 17:
+                src_port, dest_port, size, data = udp_segment(data)
+                print(TAB_1 + 'UDP Segment:')
+                print(TAB_2 + f'Source Port: {src_port}, Destination Port: {dest_port}, Length: {size}')
+                print(format_multi_line(DATA_TAB_2, data))
 
 
 #unpacking ethernet frame
@@ -55,7 +95,17 @@ def tcp_segment(data):
     return src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:] 
 
 
+def udp_segment(data):
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
 
+def format_multi_line(prefix, string , size=80):
+    size=size - len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 
 
 main()
